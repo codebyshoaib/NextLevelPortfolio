8550ae08032ac503ba52bc3e2714a0f0c3801c28
Shoaib Ud Din <60613463+codebyshoaib@users.noreply.github.com>
2023-08-26 00:23:32 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const scroll = new LocomotiveScroll({
     duration:.5
 });
 
+
+
 function firsPageAnim(){
     var tl=gsap.timeline();
 
@@ -58,6 +60,29 @@ function circleMouseFollower(xscale,yscale){
         document.querySelector("#mini-circle").style.transform=`translate(${dets.clientX}px,${dets.clientY}px) scale(${xscale},${yscale})`; //scale(${xscale},${yscale})
     })
 }
+function circleTouchFollower(xscale, yscale) {
+    const miniCircle = document.querySelector("#mini-circle");
+
+    function moveCircle(x, y) {
+        miniCircle.style.transform = `translate(${x}px, ${y}px) scale(${xscale}, ${yscale})`;
+    }
+
+    document.addEventListener('touchmove', function (event) {
+        const touch = event.touches[0]; // Get the first touch
+
+        if (touch) {
+            const x = touch.clientX;
+            const y = touch.clientY;
+            moveCircle(x, y);
+        }
+    }, { passive: false }); // Allow preventing default behavior
+
+    // Reset the circle's position when touch ends
+    
+}
+
+
+circleTouchFollower(1, 1);
 
 circleskew();
 circleMouseFollower();
@@ -65,49 +90,106 @@ firsPageAnim();
 
 
 //image animations
-document.querySelectorAll('.elem')
-.forEach(function(elem){
+document.querySelectorAll('.elem').forEach(function(elem){
     var rotate = 0;
-  var diffrot = 0;
+    var diffrot = 0;
 
-  elem.addEventListener("mouseleave", function (dets) {
-    gsap.to(elem.querySelector("img"), {
-      opacity: 0,
-      ease: Power3,
-      duration: 0.5,
-    });
-    gsap.to(elem.querySelector("h1"),{
-        opacity:.5,
-        ease:Power3,
-        duration: 0.8,
-    });
-  });
-    elem.addEventListener("mousemove",function(dets){
-
-        // var diff=dets.clientY - elem.getBoundingClientRect().top;
-        // diff=diff-diff; //!!!!!to make follower centered
-       
-        gsap.to(elem.querySelector("h1"),{
-            opacity:0.2,
-            ease:Power3,
+    function onMouseLeave() {
+        gsap.to(elem.querySelector("img"), {
+            opacity: 0,
+            ease: "power3",
+            duration: 0.5,
         });
-        var lft=dets.clientX;
-        lft=lft-lft/3;
-        diffrot = dets.clientX - rotate;
-        rotate = dets.clientX;
-        gsap.to(elem.querySelector("img"),{
-            opacity:1,
-            ease:Power3,
-            top:0,
-            left:lft,
-            right:lft,
+        gsap.to(elem.querySelector("h1"), {
+            opacity: 0.5,
+            ease: "power3",
+            duration: 0.8,
+        });
+    }
+
+    function onMouseMove(event) {
+        gsap.to(elem.querySelector("h1"), {
+            opacity: 0.2,
+            ease: "power3",
+        });
+
+        var lft = event.clientX;
+        lft = lft - lft / 3;
+        diffrot = event.clientX - rotate;
+        rotate = event.clientX;
+
+        gsap.to(elem.querySelector("img"), {
+            opacity: 1,
+            ease: "power3",
+            top: 0,
+            left: lft,
+            right: lft,
             rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
-           
         });
-        
+    }
+
+    elem.addEventListener("mouseleave", onMouseLeave);
+
+    elem.addEventListener("mousemove", onMouseMove);
+
+    elem.addEventListener("touchend", onMouseLeave);
+
+    elem.addEventListener("touchmove", function(event) {
+        event.preventDefault(); // Prevent default touch move behavior
+
+        // Use the first touch in the touches array
+        var touch = event.touches[0];
+
+        if (touch) {
+            onMouseMove(touch);
+        }
     });
 
 });
+
+// document.querySelectorAll('.elem')
+// .forEach(function(elem){
+//     var rotate = 0;
+//   var diffrot = 0;
+
+//   elem.addEventListener("mouseleave", function (dets) {
+//     gsap.to(elem.querySelector("img"), {
+//       opacity: 0,
+//       ease: Power3,
+//       duration: 0.5,
+//     });
+//     gsap.to(elem.querySelector("h1"),{
+//         opacity:.5,
+//         ease:Power3,
+//         duration: 0.8,
+//     });
+//   });
+//     elem.addEventListener("mousemove",function(dets){
+
+//         // var diff=dets.clientY - elem.getBoundingClientRect().top;
+//         // diff=diff-diff; //!!!!!to make follower centered
+       
+//         gsap.to(elem.querySelector("h1"),{
+//             opacity:0.2,
+//             ease:Power3,
+//         });
+//         var lft=dets.clientX;
+//         lft=lft-lft/3;
+//         diffrot = dets.clientX - rotate;
+//         rotate = dets.clientX;
+//         gsap.to(elem.querySelector("img"),{
+//             opacity:1,
+//             ease:Power3,
+//             top:0,
+//             left:lft,
+//             right:lft,
+//             rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+           
+//         });
+        
+//     });
+
+// });
 
 //Extended MEnu
 
@@ -134,7 +216,7 @@ function menuExtend(){
 }
 menuExtend();
 
-/*Mobile pop up*/
+
 function mobilePopup() {
     var menu_trigger = document.querySelector('#menu-trigger');
     var mobile_popup = document.querySelector("#mobile-popup");
@@ -163,7 +245,7 @@ function mobilePopup() {
         closeMobilePopup();
     });
 
-   
+    // Close the popup if the window is resized beyond the mobile breakpoint
     window.addEventListener('resize', function () {
         if (window.innerWidth > 768) {
             closeMobilePopup();
@@ -171,5 +253,22 @@ function mobilePopup() {
     });
 }
 
-
+// Call the function to set up the event listener
 mobilePopup();
+document.addEventListener("click", function () {
+    const miniCircle = document.querySelector('#mini-circle');
+    
+    
+    miniCircle.style.width = '25px';
+    miniCircle.style.height = '25px';
+    miniCircle.style.backgroundColor = 'white';
+    miniCircle.style.transition = 'width 1s ease, height 1s ease, background-color 1s ease';
+
+    
+    setTimeout(function () {
+        miniCircle.style.width = ''; 
+        miniCircle.style.height = ''; 
+        miniCircle.style.backgroundColor = ''; 
+        miniCircle.style.transition = 'width 1s ease, height 1s ease, background-color 1s ease';
+    }, 1000);
+});
